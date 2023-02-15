848bd0a3a0e11be0c932825bcd1e31e465a1aff3
RaviTejaGanti <ravi.aviate@gmail.com>
2023-02-15 23:05:21 +0000

    Random walk code & Image
    Generating Random walk using sample space and simple if & else conditions

---
+++ 2D Random Walk Code.R
@@ -0,0 +1,40 @@
+
+library(ggplot2)
+library(gganimate)
+n <- 250
+df2 <- as.data.frame(matrix(NA,n,5))
+colnames(df2) <- c("Step", "result.x","result.y", "position.x","position.y")
+df2$Step[1] <- 0
+df2$result.x[1] <- 0
+df2$position.x[1] <- 0
+df2$result.y[1] <- 0
+df2$position.y[1] <- 0
+i <- 2
+for (i in 2:n){
+  result.x <- sample(1:6,1,replace = TRUE)
+  if (result.x<=4){
+    Step = 1
+  } else {
+    Step = -1
+  }
+  df2$result.x <- Step
+  df2$position.x[i] <- df2$position.x[i-1] + Step
+  result.y <- sample(1:6,1,replace=TRUE)
+  if (result.y<=4){
+    Step = 1
+  } else {
+    Step = -1
+  }
+  df2$result.y <- Step
+  df2$position.y[i] <- df2$position.y[i-1] + Step
+  df2$Step[i] <- i-1
+  i < i+1
+}
+#df2 <- df[complete.cases(df2),]
+
+positions <- data.frame(df2$position.x,df2$position.y)
+colnames(positions) <- c("X","Y")
+
+random_2d <- ggplot(positions, aes(x = X, y = Y),type=b,pch=19) + geom_path()+ theme_minimal()
+random_2d
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 2D Random Walk Using R
  Random Walk using R
+
+This is an example of creating a Random walk using R programming.
+Here we have tried to build a data frame that consists of the steps taken in the walk.
+These steps are generated based on a condition that is in turn dependent upon a random value picked up from a sample space.
+
+This code follows these instructions:
+1. Initiating the initial steps
+2. If the random value of dice thrown is satisfying a condition ( say <= 4 or >= 3) then we mode if forward which is +1 else backward -1
+3. This is iterated over and over again until the loop reaches its maximum limit.
+4. Plotting the final positions of the is the graph generated which is also called as "2D Random Walk"
